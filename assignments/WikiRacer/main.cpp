7d9bca3f90aadc4aaf45f2fd716b97a9c8428e63
f1a3h <ihlwer@outlook.com>
2023-12-03 17:52:43 +0800

    finish WikiRacer Part B

--- assignments/WikiRacer/main.cpp
+++ assignments/WikiRacer/main.cpp
@@ -41,9 +41,7 @@ using std::unordered_set;   using std::cin;
 // BEGIN STUDENT CODE HERE
 int numCommonLinks(const unordered_set<string>& curr_set, const unordered_set<string>& target_set) {
     // replace all of these lines!
-    (void) target_set;
-    (void) curr_set;
-    return 0; 
+    return std::count_if(curr_set.begin(), curr_set.end(), [&target_set](string link) { return target_set.count(link) > 0; });
 }
 // END STUDENT CODE HERE
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -65,11 +63,11 @@ vector<string> findWikiLadder(const string& start_page, const string& end_page) 
     // BEGIN STUDENT CODE HERE
     auto cmp_fn = [&w, &target_set](const vector<string>& left, const vector<string>& right) {
         // replace all of these lines.
-        (void) w;
-        (void) target_set;
-        (void) left;
-        (void) right;
-        return false; // replace this line! make sure to use numCommonLinks.
+        string page1 = left.back();
+        string page2 = right.back();
+        int num1 = numCommonLinks(w.getLinkSet(page1), target_set);
+        int num2 = numCommonLinks(w.getLinkSet(page2), target_set);
+        return num1 < num2; // replace this line! make sure to use numCommonLinks.
     };
     // END STUDENT CODE HERE
     ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -83,10 +81,7 @@ vector<string> findWikiLadder(const string& start_page, const string& end_page) 
     // BEGIN STUDENT CODE HERE
     // something like priority_queue<...> queue(...);
     // please delete ALL 4 of these lines! they are here just for the code to compile.
-    std::priority_queue<vector<string>> queue;
-    throw std::invalid_argument("Not implemented yet.\n");
-    return {};
-
+    priority_queue<vector<string>, vector<vector<string>>, decltype(cmp_fn)> queue(cmp_fn);
     // END STUDENT CODE HERE
     ///////////////////////////////////////////////////////////////////////////////////////////////////
 
